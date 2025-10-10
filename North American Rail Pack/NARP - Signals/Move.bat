8ec2947069bf893651d230c980a8a927e8380540
Ragin1924 <zacm400@gmail.com>
2025-10-10 12:51:29 +1100

    American Signal test

---
+++ North American Rail Pack/NARP - Signals/Move.bat
@@ -0,0 +1,3 @@
+cd cd C:\Users\Zac\Desktop\Ragin's Sets\NARP - Signals\NARP - Signals
+move NARPS.grf C:\Users\Zac\Documents\OpenTTD\newgrf
+pause
---
+++ North American Rail Pack/NARP - Signals/NARPS Compile.bat
@@ -0,0 +1,3 @@
+cd C:\Users\Zac\Desktop\Ragin's Sets\NARP - Signals\ORPS
+nmlc -c --grf NARPS.grf NARPS.nml
+pause
