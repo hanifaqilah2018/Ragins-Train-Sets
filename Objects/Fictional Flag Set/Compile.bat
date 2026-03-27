d90c017155e82344fb48582ee7fad1d3ddf15cfa
Ragin1924 <zacm400@gmail.com>
2026-03-27 22:37:42 +1100

    Flags

---
+++ Objects/Fictional Flag Set/Compile.bat
@@ -0,0 +1,3 @@
+cd C:\Users\Zac\Desktop\Openttd Stuff\Ragins-Train-Sets\Objects\Fictional Flag Set
+nmlc -c --grf FicFlags.grf FicFlags.nml
+pause
---
+++ Objects/Fictional Flag Set/Move.bat
@@ -0,0 +1,3 @@
+cd C:\Users\Zac\Desktop\Openttd Stuff\Ragins-Train-Sets\Objects\Fictional Flag Set
+move FicFlags.grf C:\Users\Zac\Documents\OpenTTD\newgrf
+pause
