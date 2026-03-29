2fded855dc6d709e61594eab632e298b7eeded83
Ragin1924 <zacm400@gmail.com>
2026-03-29 17:46:22 +1100

    Steamers

---
+++ Oceanian Rail Pack/ORP - Steam Locomotives/Compile.bat
@@ -0,0 +1,3 @@
+cd C:\Users\Zac\Desktop\Openttd Stuff\Ragins-Train-Sets\Oceanian Rail Pack\ORP - Steam Locomotives
+nmlc -c --grf ORPSL.grf ORPSL.nml
+pause
---
+++ Oceanian Rail Pack/ORP - Steam Locomotives/Move.bat
@@ -0,0 +1,3 @@
+cd C:\Users\Zac\Desktop\Openttd Stuff\Ragins-Train-Sets\Oceanian Rail Pack\ORP - Steam Locomotives
+move ORPSL.grf C:\Users\Zac\Documents\OpenTTD\newgrf
+pause
