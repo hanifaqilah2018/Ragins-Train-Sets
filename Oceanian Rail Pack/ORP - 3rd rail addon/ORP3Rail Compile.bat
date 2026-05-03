8c862eb4ef4c610e0adc97c24838c8462d425d74
Ragin1924 <zacm400@gmail.com>
2026-05-03 18:16:27 +1000

    NARP Updates and code cleanup

--- Oceanian Rail Pack/ORP - 3rd rail addon/ORP3Rail Compile.bat
+++
@@ -1,3 +0,0 @@
-cd C:\Users\Zac\Desktop\Ragin's Sets\ORP3Rail
-nmlc -c --grf ORP3Rail.grf ORP3Rail.nml
-pause
--- Oceanian Rail Pack/ORP - Steam Locomotives/Move.bat
+++
@@ -1,3 +0,0 @@
-cd C:\Users\Zac\Desktop\Openttd Stuff\Ragins-Train-Sets\Oceanian Rail Pack\ORP - Steam Locomotives
-move ORPSL.grf C:\Users\Zac\Documents\OpenTTD\newgrf
-pause
