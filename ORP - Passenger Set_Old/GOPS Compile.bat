889e0a36ccf2dba634ac2c2bf40f990d97f9358f
hanifaqilah2018 <muhammadhanifaqilah@students.undip.ac.id>
2026-08-14 13:36:37 +0700

    Removed old GOPS

--- ORP - Passenger Set_Old/GOPS Compile.bat
+++
@@ -1,3 +0,0 @@
-cd C:\Users\Zac\Desktop\Ragin's Sets\GOPS
-nmlc -c --grf GOPS.grf GOPS.nml
-pause
--- ORP - Passenger Set_Old/Move.bat
+++
@@ -1,3 +0,0 @@
-cd C:\Users\Zac\Desktop\Ragin's Sets\GOPS
-move GOPS.grf C:\Users\Zac\Documents\OpenTTD\newgrf
-pause
